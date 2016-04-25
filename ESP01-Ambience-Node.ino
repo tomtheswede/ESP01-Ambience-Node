df3221d307d36ed90a97be9f34193c6e9f935197
Thomas Friberg <tomtheswede@gmail.com>
2016-04-25 12:37:16 +0800

    Overhaul

--- ESP01-Ambience-Node.ino
+++ ESP01-Ambience-Node.ino
@@ -1,26 +1,24 @@
-/*  This is a sketch which uses an ESP8266 ESP-01 to report temperature and humidity
- *  readings from pin 2 to both a local network PC via UDP broadcast and to a
- *  ThingSpeak channel for cloud access.
- *  Variables to configure:
- *    ssid, password, channelKey, reportInterval, sensorID1, sensorID2, localPort, ipAdd
-*   Edits by Thomas Friberg
+/*  
+ *   For a DHT11 or DHT22 temperature sensor with an ESP-01 chip.
+ *   Code by Thomas Friberg (https://github.com/tomtheswede)
+ *   Updated 25/04/2016
  */
 
 // Import ESP8266 libraries
 #include <ESP8266Wifi.h>
 #include <WiFiUdp.h>
+#include <DHT.h> //DHT sensor libraries
 
-//DHT sensor libraries
-#include <DHT.h>
 #define DHTPIN 2 // Pin number - Pin 14 for ESP-12, Pin 2 for ESP-01
 //#define DHTTYPE DHT11 //Comment this out if you're using a DHT22
 #define DHTTYPE DHT22 // Use DHT11 sensor OR DHT22 where more accuracy required
 DHT dht(DHTPIN, DHTTYPE, 15); //Initialize DHT sensor
 
 //Sensor IDs
-const char* sensorID1 = "TEM002"; //Name of sensor
-const char* sensorID2 = "HUM002"; //Name of sensor
-const char* devID = "Temp Sensor"; //Name of sensor
+const char* sensorID1 = "TEM001"; //Name of sensor
+const char* sensorID2 = "HUM001"; //Name of sensor
+const char* devID = "Bedroom Sensor"; //Name of sensor
+const int loopDelay = 600000; //Delay after each loop - 600000 is 10 minutes
 
 // WiFi parameters
 const char* ssid = "TheSubway";
@@ -34,8 +32,6 @@ const char* ipAdd = "192.168.0.100"; //Server address
 WiFiClient client;
 WiFiUDP Udp;
 
-//Loop variables
-const int loopDelay = 600000; //Delay after each loop
 
 
 void setup()
@@ -56,37 +52,35 @@ void setup()
   Serial.print(" on port ");
   Serial.println(localPort);
 
-  sendUdpValue("REG",sensorID1,devID);
-  sendUdpValue("REG",sensorID2,devID);
+  SendUdpValue("REG",sensorID1,devID);
+  SendUdpValue("REG",sensorID2,devID);
 
   dht.begin(); //Initialize DHT
 }
 
 
 void loop() {
-  //Thermometer and Hygrometer sensor code -------------------------
+  //Thermometer and Hygrometer sensor code ---------
   String humidityValue = String(dht.readHumidity(),1); // Reading humidity, truncate at 1 dp
   String tempValue = String(dht.readTemperature(),1); // Read temperature as Celsius, truncate at 1dp
-  sendUdpValue("LOG",sensorID1,tempValue);
-  sendUdpValue("LOG",sensorID2,humidityValue);
+  SendUdpValue("LOG",sensorID1,tempValue);
+  SendUdpValue("LOG",sensorID2,humidityValue);
 
   delay(loopDelay);  //Update temperature, wait 10 minutes (600000 seconds).
 }
 
 
-
-void sendUdpValue(String msgType, String sensorID, String value) {
+void SendUdpValue(String type, String sensorID, String value) {
   //Print GPIO state in serial
-  Serial.print("Value sent via UDP is: ");
-  Serial.println(msgType + "," + sensorID + "," + value);
+  Serial.print("Value sent via UDP: ");
+  Serial.println(type + "," + sensorID + "," + value);
 
   // send a message, to the IP address and port
   Udp.beginPacket(ipAdd,localPort);
-  Udp.print(msgType);
+  Udp.print(type);
   Udp.write(",");
   Udp.print(sensorID);
   Udp.write(",");
   Udp.print(value); //This is the value to be sent
   Udp.endPacket();
 }
-
